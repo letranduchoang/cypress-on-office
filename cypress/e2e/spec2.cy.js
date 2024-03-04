f5725c75c2b2a1a5809d72d5b6df71b4087f6beb
letranduchoang <40495320+letranduchoang@users.noreply.github.com>
2024-03-04 09:28:38 +0700

    add project

---
+++ cypress/e2e/spec2.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+
+//const cypress = require("cypress");
+
+describe('My First Test Suite', function () {
+
+    it('My FirstTest case', function () {
+
+
+        cy.visit("/")
+
+
+
+
+
+
+    })
+
+
+
+})
