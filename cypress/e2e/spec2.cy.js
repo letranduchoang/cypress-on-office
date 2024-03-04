645ad0c6ed0b6980f61be50c0ada8811143619ba
letranduchoang <40495320+letranduchoang@users.noreply.github.com>
2024-03-04 14:37:25 +0700

    update

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -6,10 +6,17 @@ describe('My First Test Suite', function () {
 
     it('My FirstTest case', function () {
 
-
         cy.visit("/")
-
-
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        //selenium get hit url in browser, cypress get acts like findElement of selenium
+        cy.get('.product').should('have.length', 5)
+        cy.get('.product:visible').should('have.length', 4)
+        //Parent child chaining
+        cy.get('.products').as('productLocator')
+        cy.get('@productLocator').find('.product').should('have.length', 4)
+        cy.get(':nth-child(3) > .product-action > button').click()
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
 
 
 
